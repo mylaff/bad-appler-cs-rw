9a60222bee091e33fdbc6fe272e8a254f093b516
mylaff <43293264+mylaff@users.noreply.github.com>
2021-08-28 21:24:22 +0300

    New Bitmap source system
    Aimed to provide Bitmaps from different sources it's goal is also to prevent preload of all Bitmap collections into RAM as it was before, and instead feed them in Enumerator way when they are requested. Some kind of Lazy initialization for bitmaps.

---
+++ BadAppler/BadAppler/Providers/FolderBitmapSource.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Drawing;
+using System.Collections;
+using System.Collections.Generic;
+using BadAppler.Utils;
+
+
+namespace BadAppler.Providers
+{
+    class FolderBitmapSource : IReadOnlyList<Bitmap>
+    {
+        private List<string> imageFiles;
+
+        private GarbageCollectionController gcController;
+
+        public Bitmap this[int index] => getBitmapFromPath(imageFiles[index]);
+
+        public int Count => imageFiles.Count;
+
+        public static Bitmap getBitmapFromPath(string path)
+        {
+            if (!File.Exists(path))
+                throw new Exception("Unable to get ordered image as folder was corrupted");
+
+            return new Bitmap(Image.FromFile(path));
+        }
+
+        public IEnumerator<Bitmap> GetEnumerator() => new FolderBitmapSourceEnumerator(imageFiles.GetEnumerator(), gcController);
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        private bool isSupportedFile(string filePath)
+        {
+            string[] supportedTypes = { ".png", ".jpg" };
+            string extension = Path.GetExtension(filePath);
+
+            return supportedTypes.Any(ext => extension == ext);
+        }
+
+        public FolderBitmapSource(string folderPath, GarbageCollectionController gcControll)
+        {
+            imageFiles = Directory.GetFiles(folderPath).Where(f => isSupportedFile(f)).ToList();
+            gcController = gcControll;
+        }
+
+        public FolderBitmapSource(List<string> imageFiles, GarbageCollectionController gcControll)
+        {
+            this.imageFiles = imageFiles;
+            gcController = gcControll;
+        }
+    }
+}
---
+++ BadAppler/BadAppler/Providers/FolderBitmapSourceEnumerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+using System.Collections;
+using System.Collections.Generic;
+using BadAppler.Utils;
+
+
+namespace BadAppler.Providers
+{
+    class FolderBitmapSourceEnumerator : IEnumerator<Bitmap>
+    {
+        public Bitmap Current => FolderBitmapSource.getBitmapFromPath(pathEnumerator.Current);
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose() { }
+
+        public bool MoveNext()
+        {
+            gcController.Iterate();
+
+            return pathEnumerator.MoveNext();
+        }
+
+        public void Reset()
+        {
+            pathEnumerator.Reset();
+            gcController.Reset();
+        }
+
+        private IEnumerator<string> pathEnumerator;
+
+        private GarbageCollectionController gcController;
+
+        public FolderBitmapSourceEnumerator(IEnumerator<string> pathEnum, GarbageCollectionController gcControll)
+        {
+            pathEnumerator = pathEnum;
+            gcController = gcControll;
+        }
+    }
+}
