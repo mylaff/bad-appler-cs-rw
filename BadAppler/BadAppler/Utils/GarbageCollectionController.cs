875b24f21fa0ad77daff9fd667c1079ee2d71567
mylaff <43293264+mylaff@users.noreply.github.com>
2021-08-28 20:26:35 +0300

    GarbageCollectionController added
    This tiny bit of a controller paired with bitmap providing enumerator hugely optimizes memory allocation for bitmap objects.
    
    Though GC calls are expensive, it's better to lose here and stay on low RAM usage, than to rise up for gigabytes of RAM usage without any perks.
    
    This class is intended to be inserted into enumerators or cycles where frequent garbage collection is needed. Just setup Frequency value and hit Iterate() on every iteration of your RAM-heavy code.

---
+++ BadAppler/BadAppler/Utils/GarbageCollectionController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace BadAppler.Utils
+{
+    class GarbageCollectionController
+    {
+        private uint frequency;
+
+        public uint Frequency
+        {
+            get => frequency;
+            set
+            {
+                iteration = 0;
+                frequency = value;
+            }
+        }
+
+        private uint iteration;
+
+        public void Reset() => iteration = 0;
+
+        public void Iterate()
+        {
+            if (++iteration == Frequency)
+            {
+                GC.Collect();
+                iteration = 0;
+            }
+        }
+
+        public GarbageCollectionController(uint frequency) => Frequency = frequency;
+    }
+}
