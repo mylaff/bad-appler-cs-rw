1923bb2abc6ec361302e22a151d0ba3c2ece6570
mylaff <43293264+mylaff@users.noreply.github.com>
2021-08-28 05:47:34 +0300

    Awful cache system
    This system sucks, like realy, I know, just one huge anti-pattern, but for now I'm just too exhausted. Consider this to be a dummy plug for a real cache system.

---
+++ BadAppler/BadAppler/CacheManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using BadAppler.Base;
+using BadAppler.Encoder.Base;
+
+namespace BadAppler
+{
+    [Serializable]
+    class Cached<T, Y> 
+    {
+        public string MusicFile { get; set; }
+
+        public Sequence<T, Y> Sequence { get; set; } // Not sure that shadowing will be good here, but for now that doesn't really matter
+    }
+
+    class CachedFrames<T> : Cached<Frame<T>, FrameSequenceMeta> where T : struct
+    {
+        public new FrameSequence<T> Sequence { get; set; } 
+    }
+
+    class CachedEncoded<T, Y> : Cached<EncodedChunk<T>, Y> where T : struct
+    {
+        public new EncodedChunkSequence<T, Y> Sequence { get; set; }
+    }
+
+    class CacheManager
+    {
+        public static CachedFrames<byte> LoadRaw(string path)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            CachedFrames<byte> rawFile;
+
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+                rawFile = (CachedFrames<byte>)formatter.Deserialize(fs);
+
+            return rawFile;
+        }
+
+        public static CachedFrames<char> LoadTranslated(string path)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            CachedFrames<char> rawFile;
+
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+                rawFile = (CachedFrames<char>)formatter.Deserialize(fs);
+
+            return rawFile;
+        }
+
+        public static CachedEncoded<char, FrameSequenceMeta> LoadEncoded(string path)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            CachedEncoded<char, FrameSequenceMeta> rawFile;
+
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+                rawFile = (CachedEncoded<char, FrameSequenceMeta>)formatter.Deserialize(fs);
+
+            return rawFile;
+        }
+    }
+}
