e070fac5ea8f80b7fc26defb397de7e9a1767854
mylaff <43293264+mylaff@users.noreply.github.com>
2021-08-28 05:16:25 +0300

    ffmpeg utilities added
    IVideoHandler provides an interface to handle and process video files. FFMpegVideoHandler is its implementation aimed to work with ffmpeg binaries.

---
+++ BadAppler/BadAppler/Utils/FFMpegVideoHandler.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.Diagnostics;
+using BadAppler.Base;
+
+namespace BadAppler.Utils
+{
+    class FFMpegVideoHandler : IVideoHandler
+    {
+        private string binaryPath;
+
+        public FFMpegVideoHandler(string binaryPath)
+        {
+            EnsureFileExist(binaryPath, "ffmpeg.exe");
+
+            this.binaryPath = binaryPath;
+        }
+
+        private void ExecuteCommand(string command) => Process.Start(new ProcessStartInfo(binaryPath, command));
+
+        private void EnsureFileExist(string path, string desc="target")
+        {
+            if (!File.Exists(path))
+                throw new FileNotFoundException($"Required {desc} file wasn't found (at: {path})");
+        }
+
+        public void ExtractAudio(string source, string output)
+        {
+            EnsureFileExist(source, "source");
+            ExecuteCommand($"-i {source} -q:a 0 -map a {output}");
+        }
+
+        /// <TODO>
+        /// Do a better workaround than to use FrameSequenceMeta.
+        /// </TODO>
+        /// <param name="settings">ffmpeg settings. In sake of my laziness I left FrameSequenceMeta class as a class for settings as it contains both resolution and framerate parameters.</param>
+        public void ToFrames(string source, string outputFolder, FrameSequenceMeta settings)
+        {
+            EnsureFileExist(source, "source");
+
+            if (!Directory.Exists(outputFolder))
+                Directory.CreateDirectory(outputFolder);
+
+            ExecuteCommand($"-i {source} -r {settings.FrameRate} {outputFolder}/frame-%07d.png");
+        }
+    }
+}
---
+++ BadAppler/BadAppler/Utils/IVideoHandler.cs
@@ -0,0 +1,11 @@
+﻿using BadAppler.Base;
+
+namespace BadAppler.Utils
+{
+    interface IVideoHandler
+    {
+        public void ExtractAudio(string source, string output);
+
+        public void ToFrames(string source, string outputFolder, FrameSequenceMeta settings);
+    }
+}
